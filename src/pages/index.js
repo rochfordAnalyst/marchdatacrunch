c13371d9e3ec7d4bf758d2aca26b30b69313b964
rochfordAnalyst <brochford@fordham.edu>
2022-05-18 15:29:48 -0400

    newest

--- src/pages/index.js
+++ src/pages/index.js
@@ -92,61 +92,11 @@ const links = [
     color: "#E95800",
   },
   {
-    text: "2022 March Madness Model Features and First Upset Probabilities based on Seed",
+    text: "2022 March Madness Model Features and First Round Possible Upset Probabilities based on Seed",
     url: "https://public.tableau.com/app/profile/brian.rochford/viz/marchMadnessProbabilityModelVisuals/MarchMadnessVisuals?publish=yes",
     description:
-      "This link brings you to the github location for our presentation regarding the results of our predictive model and insights derived from those results. The probabilities our model outputted was every possible permutation of team match ups for the 2022 NCAA March Madndess Bracket (including the teams who won or lost the play-in games).",
+      "This links brings you to tableau public and the visuals show our predictive models top features based on a feature selection package in python for Random Forest, the correlation to seed, and possible upset probabilities from the first round matchups.",
     color: "#E95800",
-  },
-  {
-    text: "Edittessz Fordham Recursion - Analyzing Differences in Mortgage Markets based on Changes in Political Party Control",
-    url: "https://public.tableau.com/app/profile/brian.rochford/viz/datasetvisuals/datasetvisualization?publish=yes",
-    description:
-      "This story created in tableau visualizes this dataset from several different angles, with objective on developing a better understanding of the data. Especially the newly created binary dummy variables based on congressional districts politcal change during an election cycle.",
-    color: "#E95800",
-  },
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
   },
 ]
 
